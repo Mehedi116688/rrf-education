e10172b34ba016cf1ad4749ea582e7048f32c471
Mehedi116688 <digitalbd32@gmail.com>
2025-07-08 13:04:57 +0600

    name change

--- min.js
+++ min.js
@@ -4,20 +4,42 @@ $(document).ready(function(){
       slidesToShow: 1,
       slidesToScroll: 1,
       arrows: false,
-      dots: true
+      dots: true,
+      
     });
 });
 
+
+
 $(document).ready(function(){
-    $('.blog-silder').slick({
+    $('.blog-slider').slick({
       autoplay: true,
       slidesToShow: 3,
       slidesToScroll: 3,
       arrows: false,
       dots: true,
-      dotsClass: 'blog-dots'
+      dotsClass: 'blog-dots',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 640,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+
+          }
+        }
+      ]
     });
 });
+
+
 
 $(document).ready(function(){
     $('.course-slider').slick({
@@ -27,6 +49,23 @@ $(document).ready(function(){
       arrows: true,
       dots: false,
       prevArrow: "<span class='arrow-prev'><</span>",
-      nextArrow: "<span class='arrow-next'>></span>"
+      nextArrow: "<span class='arrow-next'>></span>",
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 640,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+
+          }
+        }
+      ]
     });
 });
